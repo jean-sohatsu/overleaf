9e4bfd377d390918028f2ff7325c70d21d12c40a
jean-sohatsu <rouge.sohatsu@gmail.com>
2025-04-04 16:58:31 +0900

    restore build-base, it's useless

--- server-ce/build-base.ps1
+++ server-ce/build-base.ps1
@@ -4,7 +4,7 @@ $HERE = Get-Location
 
 # Get the current Git commit hash and branch name
 $MONOREPO_REVISION = git rev-parse HEAD
-$BRANCH_NAME = "latest" #git rev-parse --abbrev-ref HEAD
+$BRANCH_NAME = git rev-parse --abbrev-ref HEAD
 
 # Set the environment variables for Docker image tags
 $OVERLEAF_BASE_BRANCH = "sharelatex/sharelatex-base:$BRANCH_NAME"
@@ -18,25 +18,20 @@ $OVERLEAF_TAG = "sharelatex/sharelatex:$BRANCH_NAME-$MONOREPO_REVISION"
 Copy-Item ".dockerignore" -Destination $MONOREPO_ROOT
 
 # Run the Docker build command
-#docker build `
-#  --build-arg BUILDKIT_INLINE_CACHE=1 `
-#  --progress=plain `
-#  --file "Dockerfile-base" `
-#  --pull `
-#  --cache-from $OVERLEAF_BASE_LATEST `
-#  --cache-from $OVERLEAF_BASE_BRANCH `
-#  --tag $OVERLEAF_BASE_TAG `
-#  --tag $OVERLEAF_BASE_BRANCH `
-#  $MONOREPO_ROOT
-
 docker build `
   --build-arg BUILDKIT_INLINE_CACHE=1 `
   --progress=plain `
-  --build-arg OVERLEAF_BASE_TAG `
-  --build-arg MONOREPO_REVISION `
-  --cache-from $OVERLEAF_LATEST `
-  --cache-from $OVERLEAF_BRANCH `
+  --file "Dockerfile-base" `
+  --pull `
+  --cache-from $OVERLEAF_BASE_LATEST `
+  --cache-from $OVERLEAF_BASE_BRANCH `
+  --tag $OVERLEAF_BASE_TAG `
+  --tag $OVERLEAF_BASE_BRANCH `
+  $MONOREPO_ROOT
+
+docker build `
+  --progress=plain `
   --file Dockerfile `
+  --tag $OVERLEAF_TAG `
   --tag $OVERLEAF_BRANCH `
   $MONOREPO_ROOT
-#  --tag $OVERLEAF_TAG `
